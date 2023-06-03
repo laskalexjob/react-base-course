7d4a525826fec637eef962d5afe5fd5010c0af0e
laskalex <laskalexjob@gmail.com>
2023-06-03 09:54:43 +0200

    Custom input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "Js Post Title", body: "Description" },
@@ -12,8 +13,8 @@ function App() {
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Name of post"/>
-        <input type="text" placeholder="Description of post"/>
+        <MyInput type="text" placeholder="Name of post"/>
+        <MyInput type="text" placeholder="Description of post"/>
         <MyButton disabled>Create Post</MyButton>
       </form>
       <PostList posts={posts} title={ "Posts List #1" } /> 
