aba8a37101181ab88caaf8e8fa3dd37c97e97c3f
laskalex <laskalexjob@gmail.com>
2023-06-04 15:59:12 +0200

    PostForm as separate component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,23 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+
 function App() {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
   const [posts, setPosts] = useState([
     { id: 1, title: "Js Post Title", body: "Description" },
     { id: 2, title: "Js Post Title2", body: "Description2" },
     { id: 3, title: "Js Post Title3", body: "Description3" },
   ])
-  
-  const bodyInputRef = useRef();
-  
-  const addNewPost = (e) => {
-    e.preventDefault();
-    const newPost = {
-      id: Date.now(),
-      title,
-      body
-    }
+
+  const createPost = (newPost) => {
     setPosts([...posts, newPost])
-    setTitle('')
-    setBody('')
-    console.log(newPost);
   }
 
   return (
     <div className="App">
-      <form>
-        {/* Управляемый компонент */ }
-        <MyInput
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-          type="text"
-          placeholder="Name of post"
-        />
-        {/* <input type='text' ref={bodyInputRef}/> */}
-        {/* Неуправляемый компонент */ }
-        {/* <MyInput
-          ref={bodyInputRef}
-          type="text"
-          placeholder="Description of post" /> */}
-        <MyInput
-          value={body}
-          onChange={e => setBody(e.target.value)}
-          type="text"
-          placeholder="Description of post" />
-        <MyButton onClick={addNewPost}>Create Post</MyButton> 
-      </form>
-      <PostList posts={posts} title={ "Posts List #1" } /> 
+      <PostForm create={ createPost } />
+      <PostList posts={posts} title={"Posts List #1"} />
     </div>
   );
 }
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import MyButton from "./UI/button/MyButton";
+import MyInput from "./UI/input/MyInput";
+
+const PostForm = ({create}) => {
+    const [post, setPost] = useState({ title: '', body: '' });
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        const newPost = {
+            ...post, id: Date.now()
+        }
+        create(newPost)
+        setPost({ title: '', body: '' })
+    }
+
+    return (
+        <form>
+            {/* Управляемый компонент */}
+            <MyInput
+                value={post.title}
+                onChange={e => setPost({ ...post, title: e.target.value })}
+                type="text"
+                placeholder="Name of post"
+            />
+            {/* <input type='text' ref={bodyInputRef}/> */}
+            {/* Неуправляемый компонент */}
+            {/* <MyInput
+          ref={bodyInputRef}
+          type="text"
+          placeholder="Description of post" /> */}
+            <MyInput
+                value={post.body}
+                onChange={e => setPost({ ...post, body: e.target.value })}
+                type="text"
+                placeholder="Description of post" />
+            <MyButton onClick={addNewPost}>Create Post</MyButton>
+        </form>
+    )
+}
+
+export default PostForm
